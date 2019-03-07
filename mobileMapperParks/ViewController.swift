e2f729d728527095672f854d4fb37fc3ce06448e
Brandon Kim <bkim2192@jh-mba158-16.local>
2019-03-07 15:09:17 -0600

    Added zoom button and search button, delegate and 1 method

--- mobileMapperParks/ViewController.swift
+++ mobileMapperParks/ViewController.swift
@@ -9,14 +9,44 @@
 import UIKit
 import MapKit
 
-class ViewController: UIViewController {
+//Packages
+//More would take up a lot of space
+
+class ViewController: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
-    
+    let locationManager = CLLocationManager()
+    var currentLocation: CLLocation!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        locationManager.requestWhenInUseAuthorization()
+        mapView.showsUserLocation = true
+        locationManager.delegate = self
+        locationManager.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager.startUpdatingLocation()
+        
+        
+    }
+    // func locationManager(_ manager: CLLocationManager, didExitRegion region: CLRegion) {
+        // It can track when a user has left a certain area or radius.
+   // }
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        currentLocation = locations[0]
+       var coord = currentLocation.coordinate
+        print(coord)
+        
     }
 
-
+    @IBAction func whenZoomButtonPressed(_ sender: Any) {
+        
+    }
+    
+    @IBAction func whenSearchButtonPressed(_ sender: Any) {
+    }
+    
+    
+    
+    
+    
 }
 
